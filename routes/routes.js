8ae77e68a99b89f2cf28724a26c98f6c31b64107
jreyess11 <josegabrielreyessolanojr@gmail.com>
2024-04-12 01:46:14 -0500

    Marca-Lista Deseos
    Se agregó el endpoint que retorna las cartas en la lista de deseos de cada usuario

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ import {
   listaDeseos,
   eliminarItemListaDeseos,
   agregarListaDeseos,
+  getWishedCards
 } from "../controllers/controller.js";
 import { inventario, vitrina, enviar } from "../controllers/dummy.js";
 
@@ -14,6 +15,7 @@ const router = Router();
 router.post("/", listaDeseos);
 router.post("/agregar", agregarListaDeseos);
 router.post("/eliminar/", eliminarItemListaDeseos);
+router.post("/getWishedCards", getWishedCards);
 router.post("/dummy/getCard", inventario);
 router.post("/dummy/prices", vitrina);
 router.post("/dummy/ADD-CARD", enviar);
